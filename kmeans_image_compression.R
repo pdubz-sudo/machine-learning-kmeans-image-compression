bfa7062ad3744f4563b0d48b2b77c4ddad0032fc
Phillp Månsson <34268692+wee-analyze@users.noreply.github.com>
2018-02-09 22:04:36 +0100

    Update kmeans_image_compression.R

--- kmeans_image_compression.R
+++ kmeans_image_compression.R
@@ -34,7 +34,7 @@ K_findings = kmeans(df_original_colors, centers = k, iter.max = max_iter, nstart
 df_original_colors[,"cluster"] <- K_findings$cluster
 # or df_original_colors$cluster <- K_findings$cluster
 
-# Make dataframe of all clusters with their kmeans center values
+# Make dataframe of all clusters with their kmeans centroid center values
 df_kmeans_clusters = data.frame(cluster = 1:nrow(K_findings$centers), 
                             red_cluster = K_findings$centers[ ,"red"],
                             blue_cluster = K_findings$centers[ ,"blue"],
@@ -43,10 +43,10 @@ df_kmeans_clusters = data.frame(cluster = 1:nrow(K_findings$centers),
 # Match the pixel location with the assigned color cluster for compression. 
 # Using data.table is a very efficient way to do this; although an index must be 
 # made first because the order is not retained. This can also be done with the join 
-# function with the argument type = "inner" which also retains the order; however, this 
-# function is slower when dealing with bigger datasets compared to using data.table.
-# This script uses the data.table function; to see the an implementation with the join
-# function used look at the other R script in this repository
+# function with the argument type = "inner" which also retains the order.
+# df_all_colors <- join(df_original_colors, df_kmeans_clusters, type = "inner")
+# however, this is slower when dealing with bigger datasets compared to using data.table.
+# This script uses the data.table function.
 df_original_colors[ , "index"] <- seq.int(nrow(df_original_colors))
 # or df_original_colors$index <- seq.int(nrow(df_original_colors))
 dt_original <- data.table(df_original_colors, key = "cluster")
@@ -56,7 +56,7 @@ df_all_colors <- df_all_colors[order(df_all_colors[, "index"]),] #ordering the r
 compressed_image <- matrix(df_all_colors$cluster, 
                            nrow = dim(beach)[1], ncol = dim(beach)[2]) # %>% as.cimg %>% plot
 
-# Rebuild dataframe that will be converted to cimg for imager library for plotting. 
+# Rebuild dataframe that will be converted to cimg and then plot. 
 # dataframe needs columns x,y,cc,value
 df_base <- select(df_beach, x, y, cc)
 df_color_cluster <- data.frame(value = c(df_all_colors$red_cluster, 
